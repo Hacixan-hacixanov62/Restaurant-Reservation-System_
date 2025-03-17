ddb516811b1405d04372cbb9d9b4ec9c99fad862
unknown <hajikhanih@code.edu.az>
2025-03-17 22:55:20 +0400

    I Added FinalProject Repository and First Commit Initial

---
+++ Restaurant-Reservation-System_FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurant_Reservation_System_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
